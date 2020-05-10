6b872b209acf933b4a1a47dfca10640f150a83fb
KUAI <2542862169@qq.com>
2020-05-10 15:45:49 +0800

    Revert "Revert "wrong commit""
    This reverts commit 7c0113b21a2cdf5c7a81b5866cc261e279c7665b.

--- src/main.js
+++ src/main.js
@@ -1,26 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
-import './assets/func.js'
-import {
-    GameData
-} from './assets/gameData.js'
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbvue/lib/css/mdb.min.css'
-import './assets/main.css'
+import App from './App'
+import router from './router'
+import store from './store'
 
-window.gameData = GameData;
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-window.vm = new Vue({
-    render: h => h(App),
-    data: {
-        gameData: window.gameData,
-        runData:{
-            speed:1,
-            pause:true,
-            esc:true,
-            sideBar:false
-        }
-    }
+new Vue({
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
