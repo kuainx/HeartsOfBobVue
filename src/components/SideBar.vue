1432e5b0688fabca8660def0c6ad4c672a17baa0
KUAI <2542862169@qq.com>
2020-06-30 19:57:36 +0800

    update control

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -7,62 +7,46 @@
 		<mdb-modal-body>
 			<mdb-container>
 				<mdb-row>
-					<mdb-col col="4"><img src="http://temp.im/150x200" alt="">
+					<mdb-col col="4" style="text-align:center;"><img src="http://temp.im/150x200" alt="">
 						<h4 class="leaderName">Leader.name</h4>
 					</mdb-col>
 					<mdb-col col="8">
 						<mdb-row>
-							<div class="btn-group" role="group" style="width:100%;max-height:106px;">
-								<a-button type="button" class="btn btn-lg btn-outline-primary waves-effect" style="padding: 0;max-width:94px;"><img src="http://temp.im/90x90" alt="" width="90" height="90" /></a-button>
-								<a-button type="button" class="btn btn-lg btn-primary" style="font-size: large;letter-spacing: 0.2em;" data-toggle="modal" data-target="#modalFocus">整风运动</a-button>
-							</div>
+							<mdb-btn-group style="width:100%;max-height:106px;">
+								<mdb-btn darkWaves outline="primary" style="padding: 0;max-width:94px;"><img src="http://temp.im/90x90" alt="" width="90" height="90" /></mdb-btn>
+								<mdb-btn color="primary" style="font-size: large;letter-spacing: 0.4em;">整风运动</mdb-btn>
+							</mdb-btn-group>
+						</mdb-row>
+						<mdb-row>
+							<mdb-card class="card-body" style="padding: 4px;margin: 2px;width: 100%;">
+								<mdb-card-title style="margin:0 6px;">
+									<mdb-badge color="primary" style="letter-spacing: 0.25em;font-size: medium;">班级精神</mdb-badge>
+								</mdb-card-title>
+								<mdb-card-text>
+									<mdb-btn outline="primary" style="padding: 0;"><img src="http://temp.im/60x60" alt="" width="60" height="60" /></mdb-btn>
+									<mdb-btn outline="primary" style="padding: 0;"><img src="http://temp.im/60x60" alt="" width="60" height="60" /></mdb-btn>
+								</mdb-card-text>
+							</mdb-card>
 						</mdb-row>
 					</mdb-col>
 				</mdb-row>
 				<mdb-row>
-					<mdb-col>1 of 3</mdb-col>
-					<mdb-col col="5">2 of 3 (wider)</mdb-col>
-					<mdb-col>3 of 3</mdb-col>
+					<mdb-col coll="6">
+						<mdb-btn-group style="width:100%;max-height:86px;">
+							<mdb-btn darkWaves outline="primary" style="padding: 0;max-width:74px;"><img src="http://temp.im/70x70" alt="" width="70" height="70" /></mdb-btn>
+							<mdb-btn color="primary" style="font-size: medium;letter-spacing: 0.2em;">文科主义<br />无选举</mdb-btn>
+						</mdb-btn-group>
+					</mdb-col>
+					<mdb-col col="2">
+						<div id="partyChart"></div>
+					</mdb-col>
+					<mdb-col col="4">3 of 3</mdb-col>
 				</mdb-row>
 			</mdb-container>
 		</mdb-modal-body>
-
 	</mdb-modal>
 	<a-modal v-model="$root.runData.sideBar" title="管理" :footer="null" :mask="false" :maskClosable="false">
 		<div class="container">
-
-			<!-- <div class="row">
-				<div class="col-md-4" style="max-width: 180px;padding-top: 6px;">
-					<div class="card">
-						<div class="view overlay">
-							<img src="http://temp.im/150x200" width="150" height="200" />
-						</div>
-						<div class="card-body text-center" style="padding: 0;">
-							<button type="button" class="btn btn-outline-primary waves-effect card-title" style="padding: 1px 10px;margin: 5px 0;width: 90%;font-size: medium;letter-spacing: 0.1em;">YET</button>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-8">
-					<div class="row">
-						<div class="btn-group" role="group" style="width:100%;max-height:106px;">
-							<button type="button" class="btn btn-lg btn-outline-primary waves-effect" style="padding: 0;max-width:94px;"><img src="http://temp.im/90x90" alt="" width="90" height="90" /></button>
-							<button type="button" class="btn btn-lg btn-primary" style="font-size: large;letter-spacing: 0.2em;" data-toggle="modal" data-target="#modalFocus">整风运动</button>
-						</div>
-					</div>
-					<div class="row">
-						<div class="card" style="width: 100%;">
-							<div class="card-body" style="padding: 4px;margin: 2px;width: 100%;">
-								<div class="badge badge-primary text-wrap card-title" style="margin: 6px;letter-spacing: 0.25em;font-size: medium;">班级精神</div>
-								<div class="card-text">
-									<button type="button" class="btn btn-lg btn-outline-primary waves-effect" style="padding: 0;"><img src="http://temp.im/60x60" alt="" width="60" height="60" /></button>
-									<button type="button" class="btn btn-lg btn-outline-primary waves-effect" style="padding: 0;"><img src="http://temp.im/60x60" alt="" width="60" height="60" /></button>
-									<button type="button" class="btn btn-lg btn-outline-primary waves-effect" style="padding: 0;"><img src="http://temp.im/60x60" alt="" width="60" height="60" /></button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div> -->
 			<div class="row">
 				<div class="col-md-6" style="padding: 0 8px;">
 					<div class="btn-group" role="group" style="max-height:86px;width: 100%;">
