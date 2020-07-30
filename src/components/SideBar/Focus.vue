baaa2baabd29ac2bedc4365e33b023f140827380
KUAI <2542862169@qq.com>
2020-07-30 21:02:38 +0800

    backup
    可能安装loadsh时候没有关serve
    现在run serve报错，不知道什么问题

--- src/components/SideBar/Focus.vue
+++ src/components/SideBar/Focus.vue
@@ -4,20 +4,33 @@
 		<mdb-modal-header>
 			<mdb-modal-title>聚焦</mdb-modal-title>
 		</mdb-modal-header>
-		<mdb-modal-body>...</mdb-modal-body>
+		<mdb-modal-body class="focusContainer">
+			<FocusGraph></FocusGraph>
+		</mdb-modal-body>
 	</mdb-modal>
 
 </div>
 </template>
 <script>
+import FocusGraph from './FocusGraph.vue'
 export default {
 	name: "Focus",
 	data: () => ({
 
-	})
+	}),
+	components: {
+		FocusGraph
+	}
 }
 </script>
 <style scoped>
+.focusModal {
+	overflow: hidden !important;
+}
+
+.focusContainer {
+	padding: 0;
+}
 </style>
 <style>
 .focusModal>.modal-dialog {
